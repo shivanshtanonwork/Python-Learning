4dd723b267f4b3db3963ed17147981e6ca66a397
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-02 23:04:22 +0530

    Ternary and conditional operators

--- learning.py
+++ learning.py
@@ -3,32 +3,28 @@ import sys
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-# conditonal statements
-num1 = 15
-num2 = 10
-
-if num1 < num2:
-    print("num1 is smaller")
-else:
-    print("num2 is smaller")
-
-print("End of program")
-
+# ternary operator
 age = int(input())
+status = "Adult" if age >= 18 else "Minor"
+print(status)
 
-if age >= 18:
-    print("Adult")
+# comparison operators and or 
+x = 10
+y = 15
+z = 5
+
+if x > y and x > z:
+    print(f"{x} is greatest")
+elif y > x and y > z:
+    print(f"{y} is greatest")
+elif z > x and z > y:
+    print(f"{z} is greatest")
 else:
-    print("Minor")
+    print("They may be equal nos.")
 
 
-# if elif elif else
-marks = int(input())
-if marks>=90:
-    print("Grade A")
-elif marks >= 80:
-    print("Grade B")
-elif marks >= 70:
-    print("Grade C")
+name = input()
+if len(name)>0 and (name[1] == "S" or name[-2] == "h"):
+    print("yes")
 else:
-    print("Grade F")
+    print("no")
