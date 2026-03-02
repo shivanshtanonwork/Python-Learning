364b4a64a98826f77965326ec6a818839cd268de
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-02 14:59:23 +0530

    String

--- learning.py
+++ learning.py
@@ -3,12 +3,47 @@ import sys
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-name = "Shivansh" # String Data type
-num = 10  #Integer 
-num_float = 10.628 # float
-true = True # boolean True False
-print(name+" is a Full stack Python and AI Engineer")
+# Strings
+name = "Shivansh"
+name = 'Shivansh'
+# name = """Shivansh"""
 
-arr = [1,2,3,4,5]
-for i in arr:
-    print(i)
+detail = "Shiv's age is 26"
+desc = 'He is also knowm as "Shiva"'
+print(detail + desc)
+
+first_name = "Shivansh"
+last_name = "Tandon"
+
+#concatenation
+full_name = first_name + " " + last_name + ' '
+print(3 * full_name) # repetition
+
+# length of string
+print(len(first_name))
+
+# indexing
+# we can change entire string if we want but we cannot change letter in the string 
+name = "Tandon"
+# name[2] = "r" # not allowed immutable
+print(name)
+print(name[3:])
+
+# Escape Sequences in python
+# new line
+description = "Hi I'm Shivansh Tandon \nI'm a FullStack Python AI Engineer"
+print(description)
+# tab space
+description = "Hi I'm Shivansh Tandon \tI'm a FullStack Python AI Engineer"
+print(description)
+
+# single backlash by giving \\
+description = "Hi I'm Shivansh Tandon \\I'm a FullStack Python AI Engineer"
+print(description)
+
+# Formatted Strings
+naam = "Shivansh"
+age = 26
+role = "AI FullStack Engineer"
+
+print(f"My name is {naam}, age is {age} and role is {role}")
