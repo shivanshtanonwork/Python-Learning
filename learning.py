04639db18ef4d90217104960f3f552b660e498a0
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-02 12:41:13 +0530

    Python Setup and  Variables

---
+++ learning.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.stdin = open('input.txt','r')
+sys.stdout = open('output.txt','w')
+
+name = "Shivansh" # String Data type
+num = 10  #Integer 
+num_float = 10.628 # float
+true = True # boolean True False
+print(name+" is a Full stack Python and AI Engineer")
+
+arr = [1,2,3,4,5]
+for i in arr:
+    print(i)
