73b16db91b580a9a882d46444d87d10d43dce76e
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-02 22:33:38 +0530

    conditonal statements

--- learning.py
+++ learning.py
@@ -3,23 +3,32 @@ import sys
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-# Type Conversion
-x = int(input())
-y = int(input())
-num = x+y
-print(num)
+# conditonal statements
+num1 = 15
+num2 = 10
 
-# int()
-# float()
-# str()
-# bool()
+if num1 < num2:
+    print("num1 is smaller")
+else:
+    print("num2 is smaller")
 
-# comparing operators
-num1 = 10
-num2 = 12
+print("End of program")
+
+age = int(input())
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
 
 
-print(num1 == num2)
-print(num1 >= num2)
-print(num1 <= num2)
-print(num1 != num2)
+# if elif elif else
+marks = int(input())
+if marks>=90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+else:
+    print("Grade F")
