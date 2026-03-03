d16c75ac574c18bd822ae3b7d83f977523a32526
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-03 19:59:20 +0530

    Ordereddict

--- learning.py
+++ learning.py
@@ -3,15 +3,26 @@ import sys
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-# (key, value)
+from collections import OrderedDict
 
-from collections import defaultdict
+od = OrderedDict([(1,"Shivansh"),(3,"Mom"),(2,"Papa"),(4,"Vidushi"),(5,"DaduDadi")]) #typically key and value are of same data types in OrderedDict
 
-dd = defaultdict(int)
-dd[1] ="shivansh"
-print(dd[1])
-dd["fname"] = "shivansh"
-dd['u'] = 99
-dd['list'] = [1,2,4]
-print(dd['list'])
-print(dd["xyz"]) # since it's a defaultdict initialised with int it always returns 0
+print(od)
+print(od[1])
+
+if 5 in od:
+    print(od[5])
+else:
+    print("Not in the dict")
+
+od[10] = "messi"
+print(od)
+od.move_to_end(2, last=False)
+print(od)
+
+od.popitem()
+print(od)
+
+if 10 in od:
+    print(od.pop(10))
+print(od)
