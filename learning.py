12aa13506c5ea1dbc44f5d3a0319b796edd45c81
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-03 18:58:29 +0530

    deque from collections

--- learning.py
+++ learning.py
@@ -3,8 +3,21 @@ import sys
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-arr = [5,6,1]
-print(list(reversed(arr)))
+from collections import deque
+# deque - (double-ended queue)
+dq = deque([2,3,1])
 
-arr = list(range(1,5))
-print(arr)
+dq.append(5) # adds element from right or end
+print(dq)
+
+dq.appendleft(15)
+print(dq)
+
+dq.pop() # pops out last element from right 
+print(dq)
+
+dq.extend([10,"Shivansh"])
+print(dq)
+
+dq.extendleft([1999,27])
+print(dq)
