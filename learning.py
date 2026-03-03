a261947ff9222e81290b4e05c4543a4da9695cc6
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-03 16:28:31 +0530

    Sorting

--- learning.py
+++ learning.py
@@ -3,14 +3,28 @@ import sys
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-# lists
-nums = [1,5,6,7,7,9]
-print(len(nums))
+def length(str):
+    count = 0 
+    for i in str:
+        count = count + 1
+    return count
 
-for items in nums:
-    print(items)
+name = "Shivansh"
+print(length(name))
 
-nums.append(10)
-print(nums)
-nums.clear()
-print(nums)
+arr = [1,5,6,7,4]
+# print(sorted(arr))
+print(sorted(arr,reverse=True)) # only returns new array
+
+# to change array 
+arr.sort()
+print(arr)
+
+# sort these values according to absolute values of x - it does'nt alters the array
+arr1 = [-1,5,-6,7,4]
+print(sorted(arr1, key = lambda x: abs(x)))
+print(arr1)
+
+fruits_list = ["apple", "pineapple","kiwi"]
+print(sorted(fruits_list, key=len))
+print(sorted(fruits_list,reverse=True, key=len))
