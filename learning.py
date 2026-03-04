126f7ad8e9047407341b067ffc414fc1616d6d58
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-04 22:33:05 +0530

    dict methods

--- learning.py
+++ learning.py
@@ -3,35 +3,30 @@ import sys
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-arr = [1,6,2,4]
-arr.append(7)
+# dict methods
+
+dd = {1: "Shivansh" ,2: "Shiva", 3:"Shiv"}
+
+dd[100] = "Century"
+dd.update({10:"Messi"})
+print(dd)
+
+print(dd.get(1))
+
+print(dd.get(11,"not found"))
+
+print(dd.pop(100))
+print(dd)
+
+dd.popitem()
+print(dd)
+
+arr = list(dd.keys())
+print(arr)
+arr = list(dd.values())
+print(arr)
+arr = list(dd.items())
 print(arr)
 
-arr.extend([1,4])
-print(arr)
-
-arr.insert(2, 15)
-print(arr)
-
-arr.remove(4)
-print(arr)
-
-arr.pop()
-print(arr)
-
-print(arr.index(7))
-print(arr.count(1))
-
-arr.sort()
-print(arr)
-
-arr.reverse()
-print(arr)
-
-# arr.clear()
-# print(arr)
-
-arr2 = arr.copy()
-print(arr2)
-arr2[0] = 1999
-print(arr2)
+for item in dd.items():
+    print(item[0],item[1])
