b71d4750bf1216283d85a73350c97a086f9c1d3d
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-04 21:33:34 +0530

    math modules

--- learning.py
+++ learning.py
@@ -1,27 +1,17 @@
 import sys
-import itertools
+import math
 
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-# itertools - iteration tools
+# math modules
+print(math.pi)
+print(math.e)
+print(math.pow(2,-3))
+print(math.sqrt(9))
+print(math.factorial(5))
+print(math.gcd(10,15))
 
-arr = [1,5,5,7]
-print(list(itertools.combinations(arr,2)))
-print(list(itertools.combinations_with_replacement(arr,3)))
+print(math.ceil(10.1)) # 11 it gives next integer
 
-print(list(itertools.permutations(arr,2)))
-
-arr1 = [1,2]
-arr2 = [3,4]
-print(list(itertools.product(arr1,arr2)))
-
-
-arr = list(itertools.repeat(5,6))
-print(arr)
-
-print(list(itertools.chain([1,2],[5,6],[3,4])))
-
-print(list(itertools.accumulate(arr)))
-
-print(list(itertools.accumulate(arr,lambda x,y:x*y)))
+print(math.floor(10.8))
