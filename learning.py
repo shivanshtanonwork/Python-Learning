965c22c2df025ffbdd102a0b49881c3cd364edf1
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-04 21:51:46 +0530

    list methods

--- learning.py
+++ learning.py
@@ -1,22 +1,37 @@
 import sys
-import random
 
 sys.stdin = open('input.txt','r')
 sys.stdout = open('output.txt','w')
 
-# random modules
-print(random.random())
-
-print(random.randint(1,6))
-
-print(random.randrange(0,10,2))
-
-print(random.choice([1,6,8,9]))
-
-print(random.sample([1,6,8,9],2))
-
-arr = [1,5,6,7]
-random.shuffle(arr)
+arr = [1,6,2,4]
+arr.append(7)
 print(arr)
 
-print(random.uniform(1.0, 9.0))
+arr.extend([1,4])
+print(arr)
+
+arr.insert(2, 15)
+print(arr)
+
+arr.remove(4)
+print(arr)
+
+arr.pop()
+print(arr)
+
+print(arr.index(7))
+print(arr.count(1))
+
+arr.sort()
+print(arr)
+
+arr.reverse()
+print(arr)
+
+# arr.clear()
+# print(arr)
+
+arr2 = arr.copy()
+print(arr2)
+arr2[0] = 1999
+print(arr2)
